50d9a16639e20b838a7245951c30e059250600e4
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-11-09 11:48:13 -0600

    installed bootstrap added navbar

--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,6 +1,6 @@
 export function PostsShow(props) {
   return (
-    <div>
+    <div id="posts-show">
       <h2>Title: {props.post.title}</h2>
       <p>Created At: {props.post.created_at}</p>
     </div>

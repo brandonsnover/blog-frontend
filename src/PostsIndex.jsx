081977da3666760d57f2deba6fc1a90c9353e645
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-11-09 15:10:30 -0600

    new post path in JS

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -2,23 +2,41 @@ export function PostsIndex(props) {
   console.log(props);
 
   return (
-    <div id="posts-index">
-      <h1>All posts</h1>
-      <div className="cards">
+    <div id="posts-index" className="container text-center">
+      <h1>All Posts</h1>
+      <div className="row row-cols-3" style={{ width: "60rem" }}>
         {props.posts.map((post) => (
-          <div key={post.id} className="card">
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <img src={post.image} alt="image" />
-            <div>
-              <button onClick={() => props.onShowPost(post)}>More Info</button>
+          <div key={post.id} className="col">
+            <div className="card">
+              <img src={post.image} className="card-img-top" alt="..." />
+              <h5 className="card-title">{post.title}</h5>
+              <p className="card-text">{post.body}</p>
+              <a className="btn btn-primary" onClick={() => props.onShowPost(post)}>
+                More info
+              </a>
             </div>
           </div>
         ))}
       </div>
-      <p>
-        <button onClick={props.onShowPost}>Another Button</button>
-      </p>
     </div>
   );
 }
+
+// <div id="posts-index">
+//   <h1>All posts</h1>
+//   <div className="cards">
+//     {props.posts.map((post) => (
+//       <div key={post.id} className="card">
+//         <h2>{post.title}</h2>
+//         <p>{post.body}</p>
+//         <img src={post.image} alt="image" />
+//         <div>
+//           <button onClick={() => props.onShowPost(post)}>More Info</button>
+//         </div>
+//       </div>
+//     ))}
+//   </div>
+//   <p>
+//     <button onClick={props.onShowPost}>Another Button</button>
+//   </p>
+// </div>
--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,19 +1,55 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handle submit");
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/posts.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
+
   return (
     <div id="posts-new">
-      <h1>New post</h1>
-      <form className="inputfields">
-        <div>
-          <input type="text" placeholder="Title" />
+      <h1>New Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-floating mb-3">
+          <input name="title" type="text" className="form-control" id="floatingInput" />
+          <label htmlFor="floatingInput">Title</label>
         </div>
-        <div id="bodyline">
-          <textarea type="text" placeholder="Body" />
+        <div className="form-floating mb-3">
+          <input name="body" className="form-control" id="floatingPassword" />
+          <label htmlFor="floatingInput">Body</label>
         </div>
-        <div>
-          <input type="text" placeholder="Image" />
+        <div className="form-floating mb-3">
+          <input name="image" className="form-control" id="floatingPassword" />
+          <label htmlFor="floatingInput">Image</label>
         </div>
-        <button>Add new post</button>
+        <button type="submit">Add new post</button>
       </form>
     </div>
   );
 }
+
+// <div id="posts-new">
+//   <h1>New post</h1>
+//   <form className="inputfields">
+//     <div>
+//       <input type="text" placeholder="Title" />
+//     </div>
+//     <div id="bodyline">
+//       <textarea type="text" placeholder="Body" />
+//     </div>
+//     <div>
+//       <input type="text" placeholder="Image" />
+//     </div>
+//     <button>Add new post</button>
+//   </form>
+// </div>
